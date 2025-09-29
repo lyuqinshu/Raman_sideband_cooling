c6ac8e5fafe8d475bbd3b81179eb01d5bced1f1b
Qinshu Lyu <lyuqinshu@holylogin08.rc.fas.harvard.edu>
2025-09-29 11:40:29 -0400

    commit before scanning pop size

---
+++ optimize_piecewise/XY/cluster_scripts/XY_op_run1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J XY_op_run1
+#SBATCH -p sapphire
+#SBATCH -c 36
+#SBATCH -t 0-1:00:00 
+#SBATCH --mem=4G
+#SBATCH -o py_%j.o 
+#SBATCH -e py_%j.e 
+
+module purge
+module load python
+eval "$(mamba shell hook --shell bash)"
+mamba activate RSC_sim
+
+cd $home
+cd Raman_sideband_cooling/optimize_piecewise/XY
+python XY_optimize.py
